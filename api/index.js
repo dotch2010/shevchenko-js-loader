a9e981f599e9dbadadf3f0214810b08befcb9bda
Ravarel <dotch2010@gmail.com>
2025-04-02 09:38:50 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,22 +1,15 @@
 const shevchenko = require('shevchenko');
 
 async function main() {
-  const anthroponym = {
-    givenName: 'Сай',
-    patronymicName: 'Вікторія',
-    familyName: 'Євгенівна'
+  const input = {
+    givenName: 'Тарас',
+    patronymicName: 'Григорович',
+    familyName: 'Шевченко'
   };
-
-  const gender = await shevchenko.detectGender(anthroponym); // "feminine"
-  if (gender == null) {
-    throw new Error('Failed to detect grammatical gender.');
-  }
-
-  const input = { ...anthroponym, gender };
 
   const output = await shevchenko.inVocative(input);
 
-  console.log(output); // { givenName: "Ларисо", patronymicName: "Петрівно", familyName: "Косач-Квітко" }
+  console.log(output);
 }
 
 main().catch((error) => console.error(error));
